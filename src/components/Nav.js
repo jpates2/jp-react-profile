ac5b2276480b3b0a22c7446fec47ee699bdf88aa
Jess Pates <jesspates@hotmail.co.uk>
2024-03-19 23:12:48 +0000

    mobile navbar added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,17 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
+  const mobileNav = (
+    <>
+      <div className={classes["mobile-navbar"]}>
+        <FontAwesomeIcon icon={faHouse}  className={classes["navbar-icon"]}/>
+      </div>
+    </>
+  )
+
+  const deskNav = (
+    <>
+    <div className={classes["desk-navbar"]}>
+      <FontAwesomeIcon icon={faHouse}  className={classes["desk-navbar-icon"]}/>
+      <div className={classes["desk-navbar-list"]}>
+        <div className={classes["desk-navbar-list-item"]}>About</div>
+        <div className={classes["desk-navbar-list-item"]}>Projects</div>
+        <div className={classes["desk-navbar-list-item"]}>Stack</div>
+        <div className={classes["desk-navbar-list-item"]}>Contact</div>
+      </div>
+    </div>
+  </>
+  )
+
   return (
     <>
-      <div className={classes["navbar"]}>
-        <FontAwesomeIcon icon={faHouse}  className={classes["navbar-icon"]}/>
-        <div className={classes["navbar-list"]}>
-          <div className={classes["navbar-list-item"]}>About</div>
-          <div className={classes["navbar-list-item"]}>Projects</div>
-          <div className={classes["navbar-list-item"]}>Stack</div>
-          <div className={classes["navbar-list-item"]}>Contact</div>
-        </div>
-      </div>
+      {mobileNav}
+      {deskNav}
     </>
   )
 }
