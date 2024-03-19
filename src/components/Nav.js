97d171fdce409934c58ae2c830f3e6271e6e5383
Jess Pates <jesspates@hotmail.co.uk>
2024-03-19 22:56:56 +0000

    font awesome added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,13 @@
 import classes from "./Nav.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
   return (
     <>
       <div className={classes["navbar"]}>
+        <FontAwesomeIcon icon={faHouse}  className={classes["navbar-icon"]}/>
         <div className={classes["navbar-list"]}>
-          <div className={classes["navbar-list-item"]}>Home</div>
           <div className={classes["navbar-list-item"]}>About</div>
           <div className={classes["navbar-list-item"]}>Projects</div>
           <div className={classes["navbar-list-item"]}>Stack</div>
