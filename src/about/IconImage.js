e6c1c906d05f1b9c58a25291acd2fed595bfea9d
Jess Pates <jesspates@hotmail.co.uk>
2024-03-20 13:23:37 +0000

    framer motion installed, translate and rotate added to slider icon

--- src/about/IconImage.js
+++ src/about/IconImage.js
@@ -1,11 +1,27 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
 import classes from "./IconImage.module.css";
 import profileImg from '../images/bitmoji1.png';
 
 function IconImage() {
+  const { scrollY } = useScroll();
+
+  const yRange = [0, window.innerHeight];
+  const rotationRange = [-360, 0];
+  const translateYRange = [0, window.innerHeight];
+
+  const rotation = useTransform(scrollY, yRange, rotationRange);
+  const translateY = useTransform(scrollY, yRange, translateYRange);
+
   return (
-    <div className={classes["icon-container"]}>
-      <img src={profileImg} alt="small bitmoji" className={classes["icon-image"]} />
-    </div>
+    <motion.div className={classes["icon-container"]}>
+      <motion.img
+        src={profileImg}
+        alt="small bitmoji"
+        className={classes["icon-image"]}
+        style={{ rotate: rotation, translateY: translateY }}
+
+      />
+    </motion.div>
   )
 }
 
