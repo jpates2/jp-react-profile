c59b638383c91e3fd5840aca3a12c58b032cce78
Jess Pates <jesspates@hotmail.co.uk>
2024-03-22 08:55:29 +0000

    draw on load moved to shared file, added to about section

--- src/about/AboutContainer.js
+++ src/about/AboutContainer.js
@@ -1,4 +1,5 @@
 import classes from "./AboutContainer.module.css";
+import styles from "../styles/shared.module.css";
 import { motion } from "framer-motion";
 import Slider from "./Slider";
 import AboutBox from "./AboutBox";
@@ -66,7 +67,10 @@ function AboutContainer() {
           ease: [0.17, 0.55, 0.55, 1],
         }}
       >
-        <h1>About</h1>
+        <div className={classes["about-head"]}>
+          <h1>About</h1>
+          <svg className={`${classes["about-head-underline"]} ${styles["draw-on-load-delay"]}`} xmlns="http://www.w3.org/2000/svg" id="e51fXiHKy7A1" viewBox="0 0 100 20" shapeRendering="geometricPrecision" textRendering="geometricPrecision"><path d="M4.201681,10c9.17895-7.64913,23.515843-6.46794,35.534213-5.05402c16.59815,1.95272,39.758933,9.85412,56.182473,0" fill="none" stroke="#fabb3c" strokeWidth="3"/></svg>
+        </div>
         {desktopBoxesView}
         {mobileBoxesView}
       </motion.div>
