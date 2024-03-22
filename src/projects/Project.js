a7d0750f9d123ada1c2e7b18a45449dbb939f255
Jess Pates <jesspates@hotmail.co.uk>
2024-03-22 16:10:08 +0000

    slide in added for projects

--- src/projects/Project.js
+++ src/projects/Project.js
@@ -1,9 +1,39 @@
 import classes from "./Project.module.css";
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { useRef } from "react";
+import { useInView } from 'framer-motion';
+
+const leftProjectVariant = {
+  in: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.9,
+      ease: [0.17, 0.55, 0.55, 1],
+      delay: 0.3
+    } },
+  out: { x: "-100%", opacity: 0 }
+}
+
+const rightProjectVariant = {
+  in: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.9,
+      ease: [0.17, 0.55, 0.55, 1],
+      delay: 0.3
+    }
+  },
+  out: { x: "100%", opacity: 0 }
+}
 
 function Project({ id, title, text, languages, link, github, image }) {
+  const projectRef = useRef(null);
+  const isInView = useInView(projectRef, { once: true });
   const langs = languages.join(" | ");
 
   const projectClass = Number(id[id.length - 1]) % 2 === 0 ? classes["project-right"] : classes["project-left"];
@@ -13,9 +43,21 @@ function Project({ id, title, text, languages, link, github, image }) {
   const imageClass = Number(id[id.length - 1]) % 2 === 0 ? classes["project-right-image"] : classes["project-left-image"];
 
   return (
-    <div className={`${classes["project-container"]} ${projectClass}`}>
+    <motion.div
+      className={`${classes["project-container"]} ${projectClass}`}
+      // initial={{ x: projectClass === classes["project-right"] ? '100%' : '-100%' }}
+
+      // animate={{ x: 0 }}
+      // style={{
+      //   // opacity: isInView ? 1 : 0,
+      //   transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s"
+      // }}
+      initial="out"
+      animate={isInView ? "in" : "out"}
+      variants={projectClass === classes["project-right"] ? rightProjectVariant : leftProjectVariant}
+    >
       <div className={`${classes["project-text"]} ${textClass}`}>
-        <h3>{title}</h3>
+        <h3 ref={projectRef}>{title}</h3>
         <p>{langs}</p>
         <div className={classes["project-links"]}>
           <a href={link} target="_blank" rel="noreferrer">
@@ -29,7 +71,7 @@ function Project({ id, title, text, languages, link, github, image }) {
       <div className={`${classes["project-img-container"]} ${imageClass}`}>
         {image && <img src={image} alt="project main screenshot" />}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/projects/ProjectContainer.js
+++ src/projects/ProjectContainer.js
@@ -23,7 +23,7 @@ function ProjectContainer() {
       <motion.div className={classes["project-section"]}>
         <div className={classes["project-head"]}>
           <h1>Portfolio</h1>
-          <svg className={`${classes["project-head-underline"]} ${styles["draw-on-load"]}`} xmlns="http://www.w3.org/2000/svg" id="e51fXiHKy7A1" viewBox="0 0 100 20" shapeRendering="geometricPrecision" textRendering="geometricPrecision"><path d="M4.201681,10c9.17895-7.64913,23.515843-6.46794,35.534213-5.05402c16.59815,1.95272,39.758933,9.85412,56.182473,0" fill="none" stroke="#fabb3c" strokeWidth="3"/></svg>
+          <svg className={`${classes["project-head-underline"]} ${styles["draw-on-load-delay"]}`} xmlns="http://www.w3.org/2000/svg" id="e51fXiHKy7A1" viewBox="0 0 100 20" shapeRendering="geometricPrecision" textRendering="geometricPrecision"><path d="M4.201681,10c9.17895-7.64913,23.515843-6.46794,35.534213-5.05402c16.59815,1.95272,39.758933,9.85412,56.182473,0" fill="none" stroke="#fabb3c" strokeWidth="3"/></svg>
         </div>
         <div className={classes["project-grid"]}>
           {projects}
