5a0eca10240676a802835e88e67325f047bdb4c8
Jess Pates <jesspates@hotmail.co.uk>
2024-03-24 15:02:39 +0000

    slide in animation added to certs

--- src/stack/Certificates.js
+++ src/stack/Certificates.js
@@ -1,8 +1,28 @@
 import classes from "./Certificates.module.css";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 import { CertsInfo } from "../info/certs-info";
 import Certificate from "./Certificate";
 
+const certsVariant = {
+  in: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.9,
+      ease: [0.17, 0.55, 0.55, 1],
+      delay: 0.3
+    } },
+  out: {
+    y: "100%",
+    opacity: 0
+  }
+}
+
 function Certificates() {
+  const certsRef = useRef(null);
+  const isInView = useInView(certsRef, { once: true });
+
   const certificates = CertsInfo.map(cert => (
     <Certificate
       key={cert.id}
@@ -16,11 +36,16 @@ function Certificates() {
   ))
 
   return (
-    <div className={classes["certs-section"]}>
+    <div ref={certsRef} className={classes["certs-section"]}>
       <h2>Certificates</h2>
-      <div className={classes["certs-grid"]}>
+      <motion.div
+        className={classes["certs-grid"]}
+        initial="out"
+        animate={isInView ? "in" : "out"}
+        variants={certsVariant}
+      >
         {certificates}
-      </div>
+      </motion.div>
     </div>
   )
 }
