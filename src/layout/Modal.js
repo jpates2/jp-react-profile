f2dd6e544df36ba2ad9c72fc350f6866665be01e
Jess Pates <jesspates@hotmail.co.uk>
2024-03-24 22:32:48 +0000

    modal for certs added

---
+++ src/layout/Modal.js
@@ -0,0 +1,47 @@
+import ReactDOM from "react-dom";
+import { motion, AnimatePresence } from "framer-motion";
+import classes from "./Modal.module.css";
+
+export const Backdrop = (props) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: {duration: 0.3} }}
+        exit={{ opacity: 0 }}
+        key="backdrop"
+        className={classes["backdrop"]}
+        onClick={props.onClose}
+      />
+    </AnimatePresence>
+  )
+}
+
+const ModalOverlay = (props) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0}}
+        animate={{ opacity: 1, transition: {duration: 0.5} }}
+        exit={{ opacity: 0}}
+        key="modal"
+        className={classes["modal"]}
+      >
+        <div>{props.children}</div>
+      </motion.div>
+    </AnimatePresence>
+  )
+}
+
+const portalElement = document.querySelector("#overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+    </>
+  )
+}
+
+export default Modal;
