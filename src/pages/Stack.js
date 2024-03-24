74d29ff0ae636694e1053096d5b34b20d595b707
Jess Pates <jesspates@hotmail.co.uk>
2024-03-24 14:42:38 +0000

    footer added to stack page

--- src/pages/Stack.js
+++ src/pages/Stack.js
@@ -1,11 +1,13 @@
 import Nav from "../layout/Nav";
 import StackContainer from "../stack/StackContainer";
+import Footer from "../layout/Footer";
 
 function StackPage() {
   return (
     <>
       <Nav />
       <StackContainer />
+      <Footer />
     </>
   );
 }
